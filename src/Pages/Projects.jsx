34bb77a8cdf207312c4541545c8e8fe5532684bf
gopanmodak <gopanhridoy@gmail.com>
2026-04-21 23:38:43 +0600

    add contact pages

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div>
+      <p className="text-md font-bold bg-linear-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+        My Work
+
+      </p>
+
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white text-center mt-2">
+        Featured Projects
+
+      </h1>
+
+      <div className="flex justify-center mt-4">
+        <span className="block w-40 border-b-4 border-blue-400 rounded-full"></span>
+      </div>
+    </div>
   )
 }
 
--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div>
+      <p className="text-md font-bold bg-linear-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+        What I Do
+
+      </p>
+
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white text-center mt-3">
+        My Skills
+
+      </h1>
+
+      <div className="flex justify-center mt-5">
+        <span className="block w-24 border-b-4 border-blue-400 rounded-full"></span>
+      </div>
+    </div>
   )
 }
 
